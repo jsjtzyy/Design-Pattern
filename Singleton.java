0f9947e76e7c47393f216eef8a9a43b2a08f759d
jsjtzyy <zhangyingyi1014@163.com>
2017-09-14 13:11:21 -0500

    Update Singleton.java

--- Singleton.java
+++ Singleton.java
@@ -1,3 +1,28 @@
 public class Singleton{
+    private int value; 
+    private Singleton(){         // constructor is private because it instantiates only once inside class
+        value = 0;
+    }
+    private static final Singleton instance = new Singleton(); // only one static instance is instantiated like global instance
   
+    // methods
+    public static Singleton getInstance(){
+        return instance;
+    }
+  
+    public synchronized void addValue(int amount){           // synchronized is used for multithreads working on value
+        value += amount;
+    }
+  
+    public synchronized int getValue(){
+        return value;
+    }
+  
+    public synchronized boolean getAmount(int amount){
+        if(value >= amount){
+            value -= amount;
+            return true;
+        }
+        return false;
+    }
 }

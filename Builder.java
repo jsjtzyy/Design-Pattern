33a5844eb8ea701123cef41082a297fc07effaa8
jsjtzyy <zhangyingyi1014@163.com>
2017-09-20 12:56:32 -0500

    Update Builder.java

--- Builder.java
+++ Builder.java
@@ -13,7 +13,66 @@
         Allows objects to be constructed in a multistep and varying process (as opposed to one step factories).
         Work for immutable class
 */
+import java.util.*
 
+// Animal is a immutable class  with keyword final
+public final class Animal {
+    private final String species;
+    private final int age;
+    private final List<String> favoriteFoods;
+    public Animal(String species, int age, List<String> favoriteFoods) { 
+        this.species = species;
+        this.age = age;
+        if(favoriteFoods == null) {
+            throw new RuntimeException("favoriteFoods is required"); 
+        }
+        this.favoriteFoods = new ArrayList<String>(favoriteFoods); }
+    public String getSpecies() {       // only has get methods, no set methods, because class is immutable
+        return species;
+    }
+    public int getAge() { 
+        return age;
+    }
+    public int getFavoriteFoodsCount() { 
+        return favoriteFoods.size();
+    }
+    public String getFavoriteFood(int index) { 
+        return favoriteFoods.get(index);
+    } 
+}
+
+// Design AnimalBuilder class based on Builder Pattern
+public class AnimalBuilder {   // setter methods can be owned by AnimalBuilder
+    private String species;
+    private int age;
+    private List<String> favoriteFoods;
+    public AnimalBuilder setAge(int age) { 
+        this.age = age;
+        return this;               // for chaining use
+    }
+    public AnimalBuilder setSpecies(String species) {  
+        this.species = species;
+        return this;               // "return this" is used for chaining set attributes. see example below
+    }
+    public AnimalBuilder setFavoriteFoods(List<String> favoriteFoods) { 
+        this.favoriteFoods = favoriteFoods;
+        return this;              // for chaining use
+    }
+    public Animal build() {       // build method is used for instantiate object finally
+        return new Animal(species,age,favoriteFoods);
+    }
+}
+
+// application example
 public class Builder{
-
+    public static void main(String[] args){
+        /* Approach 1 */
+        AnimalBuilder duckBuilder = new AnimalBuilder();      // new a AnimalBuilder
+        duckBuilder.setAge(4).setFavoriteFoods(Arrays.asList("grass","fish")).setSpecies("duck");  // use chaining set method
+        Animal duck = duckBuilder.build();                   // finally instantiate Animal object with above settings
+        
+        /* Approach 2 */
+        // we can also append the build() method to the chaining! 
+        Animal flamingo = new AnimalBuilder() .setFavoriteFoods(Arrays.asList("algae","insects")) .setSpecies("flamingo").build();
+    }
 }

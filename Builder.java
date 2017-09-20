7eabebb24e0f648c270856b1acbf40c4a56aaa65
jsjtzyy <zhangyingyi1014@163.com>
2017-09-19 23:20:52 -0500

    Update Builder.java

--- Builder.java
+++ Builder.java
@@ -1,3 +1,10 @@
+/*
+    Benefits:
+        Encapsulates the way a complex object is constructed.
+        Allows objects to be constructed in a multistep and varying process (as opposed to one step factories).
+        Work for immutable class
+*/
+
 public class Builder{
 
 }
